f3496c9791ebc4793737273d069313bf29d0f07a
shourya200711 <shouryaprataps617@gmail.com>
2026-08-17 18:09:02 +0530

    added day 03

---
+++ day3_celsius_to_fahrenheit.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    float celsius, fahrenheit;
+    scanf("%f", &celsius);
+    fahrenheit = (celsius * 9 / 5) + 32;
+    printf("fahrenheit=%.2f", fahrenheit);
+    return 0;
+}
