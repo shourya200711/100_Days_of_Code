2e10cc0596b4b624c336c5aa9d2b4cb7c5e2c2f8
Shourya Pratap Singh <shouryaprataps617@gmail.com>
2026-08-26 02:34:07 +0530

    initial commit

---
+++ day15_palindrom.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, original, rev = 0, rem;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while(n != 0)
+    {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n = n / 10;
+    }
+
+    if(original == rev)
+    {
+        printf("Palindrome");
+    }
+    else
+    {
+        printf("Not Palindrome");
+    }
+
+    return 0;
+}
